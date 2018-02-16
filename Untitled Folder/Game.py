331fe1ae9243cf32b56c9173e1689c8d60d2b314
tmishra3 <36511443+tmishra3@users.noreply.github.com>
2018-02-15 20:07:58 -0600

    Add files via upload

--- Untitled Folder/Game.py
+++ Untitled Folder/Game.py
@@ -45,12 +45,15 @@ for i in range (0,3):
 
 print(colored("You have 3 decks of cards. Pick a card from a deck and remember it.", 'blue', attrs=['bold']))
 print(colored("Which deck was your card in, (1,2 or 3) ? ", 'blue', attrs=['bold']))
-firstRound = input('')
+firstRound = input('') #Inputting the number which represents which deck you chose.
 print("\n")
 
-firstPick = deck("FIRST PICK",False)
+#First input where we ask for the input of what a person is choosing. Future implementations will include
+#a check for correct inputs.
 
-firstRound = str(firstRound)
+firstPick = deck("FIRST PICK",False) #Creating an empty deck that we will use temporarily to store our distributed cards.
+
+firstRound = str(firstRound) #Converting the option the person chose into a string.
 
 if (firstRound=="1"):
     orderShuffle = [1,0,2]
@@ -58,14 +61,22 @@ if (firstRound=="2"):
     orderShuffle = [0,1,2]
 if (firstRound=="3"):
     orderShuffle = [0,2,1]
+
+#The 3 if statements above are portraying how the shuffle order will occur. For example, if we choose deck 1, we keep it
+#centered (index 0) and put it between deck 2 and 3 (index 1 and 2). If we choose deck 2, we keep it between deck 1 and 3,
+#etc
     
 for i in range (0,3):
     for j in range(0,7):
         firstPick.addCard(DecksOf7[orderShuffle[i]].returnCardN(j+1),1)
 
+#Here, what we're doing is stacking the 3 decks of 7 on top of each other based upon the order specified in orderShuffle.
+#This will create a deck of 21.
+
 for i in range (0,3):
     DecksOf7[i].emptyDeck()
     
+#Emptying out all the decks of 7.
 
 counter = 0
 for j in range (0,7):
@@ -73,12 +84,20 @@ for j in range (0,7):
         DecksOf7[2-i].addCard(firstPick.returnCardN(1+counter))
         counter += 1
 
+#Now, we're redistributing the 21 cards into 3 stacks again by going, 1 2 3, 1 2 3, 1 2 3...7 times. The values are stored back
+#into the 3 decks of 7.
+
 for i in range (0,3):
     DecksOf7[i].printDeck()
+
+#We now print the 3 decks.
 
 print(colored("Which deck is your card in (1,2 or 3)? ", 'blue', attrs=['bold']))
 firstRound = input('')
 print("\n")
+
+#After this point, the code repeats. Future implementations of the code would turn the repeated code into a function that we can
+#call.
 
 firstRound = str(firstRound)
 
